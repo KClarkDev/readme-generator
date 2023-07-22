02bafb72049d3a4410cb766474dd147ed6fe7066
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-07-22 19:09:07 -0400

    Created questions list, template markdown file, and writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,74 @@
-// TODO: Include packages needed for this application
+// Import packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Title, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "projectDescription",
+    message:
+      "Provide a short description explaining the what, why, and how of your project. Some details you could include are the motivation for the project, what problem it solves, and what you learned.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message:
+      "Which type of Open Source License to you want to add to your project?",
+    choices: [
+      "Apache License 2.0",
+      "MIT License",
+      "BSD License",
+      "GNU GPLv3 License",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "If you would like other developers to contribute to your application or package, include guidelines for how to do so.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "If you wrote tests for your application, provide examples and explain how to run them.",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, markdown, (err) =>
+    err ? console.log(err) : console.log("Successfully created README file!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
---
+++ Develop/readMeTemplate.md
@@ -0,0 +1,49 @@
+# projectTitle
+
+## Description
+
+projectDescription
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+installation
+
+## Usage
+
+usage
+
+<!-- ## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well. -->
+
+## License
+
+license
+
+<!-- ## Features
+
+If your project has a lot of features, list them here. -->
+
+## How to Contribute
+
+contributing
+
+## Tests
+
+tests
+
+## Questions?
+
+github
+Please email email if you have any questions about the contents of this repository
