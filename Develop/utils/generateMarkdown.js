eea58ad928aef6417c77dc64c421eb2b94d734b7
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-07-23 14:41:22 -0400

    Added template md to generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,9 +12,62 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+
+  ## Description
+
+  ${projectDescription}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${installation}
+
+  ## Usage
+
+  ${usage}
+
+  ## License
+
+  ${license}
+
+  ## How to Contribute
+
+  ${contributing}
+
+  ## Tests
+
+  ${tests}
+
+  ## Questions?
+
+  Github profile: https://github.com/${github}
+  Please email ${email} if you have any questions about the contents of this repository
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+/* 
+
+POTENTIAL TO ADD AFTER MVP: 
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## Features
+
+If your project has a lot of features, list them here.
+*/
